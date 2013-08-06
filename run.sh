aa76050a314b5773cf310881757e2440a6872548
fbarbut <fbarbut@motion-twin.com>
2013-08-06 12:05:49 +0200

    open firefox

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ cd $pwd
 
 haxelib run openfl test neko
 #haxelib run openfl test cpp	
-haxelib run munit test -kill-browser=1
+haxelib run munit test -kill-browser true -browser firefox
 
 kill -15 $SERVER_PID
