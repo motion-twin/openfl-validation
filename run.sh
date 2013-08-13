f20f5a87055262391cfeac1c8ea5acc56f5d4b8a
fbarbut <fbarbut@motion-twin.com>
2013-08-13 10:08:57 +0200

    android

--- run.sh
+++ run.sh
@@ -1,32 +1,30 @@
-#!/bin/bash
-
-
+#!/bin/bash -x
 if [ $OSTYPE = "cygwin" ]; then
-	#windows
-    neko_server_root="/cygdrive/c/projects/munit/src/"
+        #windows
+        neko_server_root="/cygdrive/c/projects/munit/src/"		
 else
-	#linux
-	neko_server_root="build/"
+        #linux
+        neko_server_root="build/"
 fi
 
-echo $neko_server_root
+echo "using $neko_server_root as nekotools server root"
+rm $neko_server_root/tmp/results.txt
 
 pwd=$(pwd)
 cd $neko_server_root
-nekotools server 2>nekotools.log &
+nekotools server >nekotools.log &
 SERVER_PID=$!
 cd $pwd
+
 
 export DISPLAY=:10
 
 haxelib run openfl test neko
-haxelib run openfl test cpp	
+haxelib run openfl test cpp
 
 haxelib run openfl test android &
-sleep 120
+sleep 60
 
 haxelib run munit test -kill-browser -browser firefox -mlib-log all -result-exit-code
-
-
 
 kill -15 $SERVER_PID
